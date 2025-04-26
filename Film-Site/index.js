e4f2ddff7e05f60a25b1717e66c02ee8db1e8ac3
mohitkumar <mohitkumar269856@gmail.com>
2025-04-26 15:40:18 +0530

    updated film-site

--- Film-Site/index.js
+++ Film-Site/index.js
@@ -2,60 +2,87 @@ let movieListField = document.querySelector(".movie-list-div");
 let searchBar = document.getElementById("search-bar");
 let searchBtn = document.getElementById("search-btn");
 let data = [];
-const filmApi = async () => {
-  const api = await fetch("./localApi.json");
-  data = await api.json();
-  for (const film of data.data) {
-    movieListField.innerHTML += `
+let val = searchBar.value.toLowerCase();
+
+async function apiCallFxn() {
+  try {
+    const response = await fetch("./localapi.json");
+    const result = await response.json();
+    console.log(result);
+
+    data = result.movies;
+    console.log(data);
+    data.map((film) => {
+      movieListField.innerHTML += `
         <div class="movie-box-div">
-        <img src="${film.poster}" alt="movie-poster" id="poster" />
-          <h3 id="title">${film.name}</h3>
+        <img src="${film.poster_url}"  onerror="this.onerror=null; this.src='./assets/coming.webp';" alt="movie-poster" id="poster" />
+          <h3 id="title">${film.title}</h3>
           <p id="desc">
-          ${film.desc}
+          ${film.synopsis}
           </p>
+          <p id="desc">
+          Genres : ${film.genre}
+          </p>
+          <span class="span"><p id="desc">Rating :  ${film.rating}</p> <p>l</p> <p id="desc">Released : ${film.release_year}</p></span>
         </div>`;
+    });
+  } catch (error) {
+    console.error(error);
   }
-};
-filmApi();
+}
+apiCallFxn();
 
 // Search Film By Search Button
+
 const searchFilmBySearchBtn = async () => {
   movieListField.innerHTML = "";
-  const val = searchBar.value.toLowerCase();
-  console.log(val);
-  let filterData = data.data.filter((film) => film.name.toLowerCase() === val);
-  filterData.forEach((film) => {
-    movieListField.innerHTML += `
-          <div class="movie-box-div">
-          <img src="${film.poster}" alt="movie-poster" id="poster" />
-            <h3 id="title">${film.name}</h3>
-            <p id="desc">
-            ${film.desc}
-            </p>
-          </div>`;
+  data.map((film) => {
+    if (film.title.toLowerCase().includes(val)) {
+      movieListField.innerHTML += `
+        <div class="movie-box-div">
+        <img src="${film.poster_url}"  onerror="this.onerror=null; this.src='./assets/coming.webp';" alt="movie-poster" id="poster" />
+          <h3 id="title">${film.title}</h3>
+          <p id="desc">
+          ${film.synopsis}
+          </p>
+          <p id="desc">
+          Genres : ${film.genre}
+          </p>
+          <span class="span"><p id="desc">Rating :  ${film.rating}</p> <p>l</p> <p id="desc">Released : ${film.release_year}</p></span>
+        </div>`;
+    }
   });
 };
 
-// Search Film By Search Bar
 searchBtn.addEventListener("click", (e) => {
-  searchFilmBySearchBtn();
+  if (val) {
+    searchFilmBySearchBtn();
+  } else {
+    searchBar.value = "enter name first";
+  }
 });
+
+// Search Film By Search Bar
 const searchFilmBySearchBar = async () => {
   movieListField.innerHTML = "";
   const val = searchBar.value.toLowerCase();
-  for (const film of data.data) {
-    if (film.name.toLowerCase().includes(val)) {
+  data.map((film) => {
+    if (film.title.toLowerCase().includes(val)) {
       console.log(val);
       movieListField.innerHTML += `
-                <div class="movie-box-div">
-                    <img src="${film.poster}" alt="movie-poster" id="poster" />
-                    <h3 id="title">${film.name}</h3>
-                    <p id="desc">
-                    ${film.desc}
-                    </p>
-                    </div>`;
+        <div class="movie-box-div">
+        <img src="${film.poster_url}"  onerror="this.onerror=null; this.src='./assets/coming.webp';" alt="movie-poster" id="poster" />
+          <h3 id="title">${film.title}</h3>
+          <p id="desc">
+          ${film.synopsis}
+          </p>
+          <p id="desc">
+          Genres : ${film.genre}
+          </p>
+          <span class="span"><p id="desc">Rating :  ${film.rating}</p> <p>l</p> <p id="desc">Released : ${film.release_year}</p></span>
+        </div>`;
     }
-  }
+  });
 };
 searchBar.addEventListener("input", () => {
   searchFilmBySearchBar();
